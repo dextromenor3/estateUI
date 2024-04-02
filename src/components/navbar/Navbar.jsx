c77efee44503493e6977a649b491ab2508b85c23
dextromenor3 <dextromenor@gmail.com>
2024-04-02 19:12:43 +0200

    card

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,26 @@
 import "./navbar.scss";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
   return (
     <nav>
       <div className="left">
-        <a href="/" className="logo">
+        <Link to='/' className="logo">
           <img src="logo.png" alt="logo" />
           <span>VladEstate</span>
-        </a>
-        <a href="/">Home</a>
-        <a href="/">About</a>
-        <a href="/">Contact</a>
-        <a href="/">Agents</a>
+        </Link>
+        <Link to='/'>Home</Link>
+        <Link to='/'>About</Link>
+        <Link to='/'>Contact</Link>
+        <Link to='/'>Agents</Link>
       </div>
       <div className="right">
-        <a href="/">Sign in</a>
-        <a href="/" className="register">
+        <Link to='/'>Sign in</Link>
+        <Link to='/' className="register">
           Sign up
-        </a>
+        </Link>
         <div className="menuIcon">
           <img
             src="/menu.png"
@@ -28,12 +29,12 @@ function Navbar() {
           />
         </div>
         <div className={open ? "menu active" : "menu"}>
-          <a href="/">Home</a>
-          <a href="/">About</a>
-          <a href="/">Contact</a>
-          <a href="/">Agents</a>
-          <a href="/">Sign In</a>
-          <a href="/">Sign Up</a>
+          <Link to='/'>Home</Link>
+          <Link to='/'>About</Link>
+          <Link to='/'>Contact</Link>
+          <Link to='/'>Agents</Link>
+          <Link to='/'>Sign In</Link>
+          <Link to='/'>Sign Up</Link>
         </div>
       </div>
     </nav>
