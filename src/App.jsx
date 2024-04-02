79e498eda3efb18f3ea592521570ed0d7317e9e7
dextromenor3 <dextromenor@gmail.com>
2024-04-02 17:16:08 +0200

    routing + Filter comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import Navbar from "./components/navbar/Navbar";
-import "./layout.scss";
 import HomePage from "./routes/homePage/homePage";
 
-function App() {
-  return (
-    <div className="layout ">
-      <div className="navbar">
-        <Navbar />
-      </div>
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
+import LoginPage from "./routes/loginPage/loginPage";
 
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
-  );
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <HomePage /> },
+        { path: "/list", element: <ListPage /> },
+        { path: "/:id", element: <SinglePage /> },
+        { path: "/login", element: <LoginPage /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
